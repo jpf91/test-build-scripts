88529fe6ffbd2f0873753591c1a4f1024be36da9
Johannes Pfau <johannespfau@gmail.com>
2017-01-25 18:40:01 +0100

    ping test

--- build-x86.sh
+++ build-x86.sh
@@ -1,13 +1,16 @@
 startpath=$(pwd)
 echo "Started in $startpath"
+scripts=${startpath}/buildscripts
 ls -la
 whoami
+
+ping -c 1 154.16.245.132
 
 cd /home/build
 mount nfs
 
 # Setup build system
-cp ${startpath}/configs/gccbuild.json ~/.config/gccbuild.json
+cp ${scripts}/configs/gccbuild.json ~/.config/gccbuild.json
 mkdir /home/build/nfs/download
 
-${startpath}/bin/gcc-build-script download --toolchainConfig ${startpath}/configs/gcc-6/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu.json
+${scripts}/bin/gcc-build-script download --toolchainConfig ${scripts}/configs/gcc-6/x86_64-unknown-linux-gnu/x86_64-unknown-linux-gnu.json
